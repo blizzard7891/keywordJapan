89661db638a8873b8397e7871aab77b568aec545
soojin <bllizard7891@gmail.com>
2019-07-22 23:04:51 +0900

    yahoo comment crawling function

--- crawler.py
+++ crawler.py
@@ -2,21 +2,39 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 
-url = 'https://headlines.yahoo.co.jp/cm/main?d=20190718-00000113-asahi-soci'
+url = 'https://headlines.yahoo.co.jp/cm/videomain?d=20190719-00000055-ann-soci'
+webdriverFile = '/Users/osujin/Documents/python/keywordJapan/chromedriver'
 
-driver = webdriver.Chrome('/Users/osujin/Documents/python/chromedriver')
-driver.implicitly_wait(5)
-
+driver = webdriver.Chrome(webdriverFile)
+driver.implicitly_wait(2)
 driver.get(url)
-target = driver.find_element_by_id('footer')
-print(target.text)
-driver.execute_script("arguments[0].scrollIntoView(true);", target)
-time.sleep(3)
+time.sleep(1)
 
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-notices = soup.find_all('span', {'class' : 'cmtBody'})
+#target = driver.find_element_by_id('contentsBody')
+#driver.execute_script("arguments[0].scrollIntoView(true);", target)
 
-for n in notices:
-    print(n.text.strip())
-    print()
+
+pages = 1
+try:
+	while True:
+		print("{}-------".format(pages))
+		pages += 1
+
+		commentFrame = driver.find_element_by_css_selector('#main > div.mainBox > div.article.comment > div:nth-child(5) > div > iframe')
+		driver.switch_to.frame(commentFrame)
+		time.sleep(1)
+		html = driver.page_source
+		soup = BeautifulSoup(html, 'html.parser')
+		time.sleep(1)
+		notices = soup.find_all('span', {'class' : 'cmtBody'})
+		for n in notices:
+   			print(n.text.strip())
+
+		nextBtn = driver.find_element_by_css_selector('#ft > ul > li.next > a')
+		nextBtn.click()
+		time.sleep(2)
+
+except Exception as e:
+	pass
+
+
